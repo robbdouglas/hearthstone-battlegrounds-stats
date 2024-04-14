4ca69515643781b557f7da4c4ff7255e8a38aacb
robbdouglas <robert.nimmrich@icloud.com>
2024-04-14 11:23:29 +0200

    add Login and Register components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Register from "./components/Register"; // Importiere die Register-Komponente
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} /> {/* Hinzugefügt */}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,7 @@
+// LandingPage.jsx
+
 import React from "react";
+import { Link } from "react-router-dom"; // Importiere Link aus react-router-dom
 import "../styles/LandingPage.css";
 
 function LandingPage() {
@@ -9,8 +12,12 @@ function LandingPage() {
         <h2>Do you want to create a new account or do you want to sign in?</h2>
       </div>
       <div className="button-div">
-        <button className="button">Create Account</button>
-        <button className="button">Sign In</button>
+        <Link to="/register" className="button">
+          Register
+        </Link>
+        <Link to="/login" className="button">
+          Login
+        </Link>
       </div>
     </div>
   );
