fc2db649ee59120a31745a3ad0729292cd6fb6f1
robbdouglas <robert.nimmrich@icloud.com>
2024-04-14 11:07:41 +0200

    create data dir and add userData.js

---
+++ data/userData.js
@@ -0,0 +1,12 @@
+// data/userData.js
+
+let userData = [
+  {
+    email: "test@test.com",
+    password: "testitest",
+  },
+  {
+    email: "test2@test.com",
+    password: "testitest2",
+  },
+];
