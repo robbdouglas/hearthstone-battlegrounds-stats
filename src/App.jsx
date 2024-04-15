832363de7552477143ae0d0de6dbffed938aaf03
robbdouglas <robert.nimmrich@icloud.com>
2024-04-15 15:26:11 +0200

    remove ChangeEmail from App.jsx and routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import PlayerStats from "./components/PlayerStats";
 import StatsDetails from "./components/StatsDetails"; 
 import ChangeUserName from "./components/ChangeUserName";
 import ChangePassword from "./components/ChangePassword";
-import ChangeEmail from "./components/ChangeEmail";
 import DeleteAccount from "./components/DeleteAccount";
 
 function App() {
@@ -34,7 +33,6 @@ function App() {
         <Route path="stats-details" element={<StatsDetails />} /> 
         <Route path="/change-username" element={<ChangeUserName />} />
         <Route path="/change-password" element={<ChangePassword />} />
-        <Route path="/change-email" element={<ChangeEmail />} />
         <Route path="/delete-account" element={<DeleteAccount />} />
         <Route path="*" element={<h1>ERROR 404: Not Found</h1>} />
       </Routes>
