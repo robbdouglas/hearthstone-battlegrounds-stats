9486123ec2152e03daa46f70c297c53b6f95cf8f
robbdouglas <robert.nimmrich@icloud.com>
2024-04-16 09:38:20 +0200

    edit Login.jsx to login with registered data

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "../styles/Login.css";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/users/login", {
+        username,
+        password,
+      });
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      window.location.href = "/dashboard";
+    } catch (error) {
+      setError("Username or password is incorrect!");
+    }
+  };
+
   return (
     <div>
       <div className="login-welcome">
@@ -10,13 +30,15 @@ function Login() {
         <h2>Sign in by entering your account data!</h2>
       </div>
       <div className="login-form">
-        <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="username">Username:</label>
           <input
             type="text"
             id="username"
             name="username"
             placeholder="Enter username..."
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
           <label htmlFor="password">Password:</label>
@@ -25,9 +47,11 @@ function Login() {
             id="password"
             name="password"
             placeholder="Enter password..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
-
+          {error && <p className="error-message">{error}</p>}
           <button type="submit">Login</button>
         </form>
         <div className="home-button-div">
